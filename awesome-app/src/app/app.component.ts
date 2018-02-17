c658c082c23fb5a36ec40a457eff02c65d78252c
sabuj <md.sabuj.sarker@gmail.com>
2018-02-17 22:45:02 +0600

    Article 10: Two Way Data Binding an Pipes in Angular

--- awesome-app/src/app/app.component.ts
+++ awesome-app/src/app/app.component.ts
@@ -12,31 +12,9 @@ interface ISocialLink{
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = "Md. Sabuj Sarker";
-  profession = "Software Engineer";
-  website = "http://sabuj.me";
-  social_links: ISocialLink[] = [
-    { title: "Facebook", link: "https://facebook.com/SabujXiP", is_active: true },
-    { title: "Twitter", link: "https://twitter.com/SabujXi", is_active: false },
-    { title: "Github", link: "https://github.com/SabujXi", is_active: true }
-  ];
-  current_quote = "";
-  quotes = [];
+  input_data: string = "";
 
-  removeLink(idx){
-    this.social_links.splice(idx, 1);
-  }
-
-  saveQuote(){
-    if (this.current_quote){
-      this.quotes.push(this.current_quote);
-    }
-  }
-
-  inputChanged(evt){
-      this.current_quote = evt.target.value;
-  }
-
-  constructor(){
+  react(){
+    this.input_data += " : changed!";
   }
 }
