7fe0b324a4aea82422371c5101e97bc930be238b
sabuj <md.sabuj.sarker@gmail.com>
2018-02-17 22:13:59 +0600

    Article 03: Creating Your First Simple Angular Application

--- awesome-app/src/app/app.component.ts
+++ awesome-app/src/app/app.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 
-interface ISocialLink{
-  title: string;
-  link: string;
-  is_active: boolean;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  input_data: string = "";
-  
-  react(){
-    this.input_data += " : changed!";
-  }
+  name = "Md. Sabuj Sarker";
+  profession = "Software Engineer";
+  website = "http://sabuj.me";
 }
