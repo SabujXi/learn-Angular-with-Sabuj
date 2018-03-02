1465af5e829701d2a01d19cf1cd1d57bca014f2e
sabuj <md.sabuj.sarker@gmail.com>
2018-03-02 18:58:41 +0600

    Article 013: ... todo_array changed to tasks and uses Tasks interface now; other methods changed accordingly

--- awesome-app/src/app/app.component.ts
+++ awesome-app/src/app/app.component.ts
@@ -1,22 +1,40 @@
 import { Component } from '@angular/core';
 
+interface Task{
+  title: string,
+  is_canceled: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  todo_array: Array<string> = [
-    "Go home",
-    "Take a nap",
-    "Start learning Angular with Sabuj"
-  ]
+  tasks: Array<Task> = [
+    {
+      title: "Go home",
+      is_canceled: false
+    },
+    {
+      title:"Take a nap",
+      is_canceled: false
+    },
+    {
+      title: "Start learning Angular with Sabuj",
+      is_canceled: false
+    }
+  ];
 
   clearToDo(){
-    this.todo_array.splice(0);
+    this.tasks.splice(0);
   }
 
   addTask(value: string){
-    this.todo_array.push(value);
+    this.tasks.push(
+      {
+        title: value,
+        is_canceled: false
+      });
   }
 }
