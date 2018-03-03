b8c913bcd2ebf8873c30ec072e18e006aabd2b72
sabuj <md.sabuj.sarker@gmail.com>
2018-03-03 20:56:23 +0600

    Article 020: Developing a To-Do Application with Angular 5 - part 10

--- awesome-app/src/app/app.component.ts
+++ awesome-app/src/app/app.component.ts
@@ -1,9 +1,65 @@
 import { Component, TemplateRef, Input, ElementRef } from '@angular/core';
 
-interface Task{
-  title: string,
-  is_canceled: boolean,
-  f_idx: number
+class Task{
+  title: string;
+  is_canceled: boolean = false;
+  displayable: boolean = true;
+
+  constructor(title: string, private component){
+    this.title = title;
+  }
+
+  get display_title(){
+    let filter_by = this.component.filter_by;
+    var highlighted_title = this.title;
+
+    if (filter_by !== ""){
+      let title = this.title;
+      let title_lower = title.toLowerCase();
+      if (title_lower.includes(filter_by)){
+        let filter_len = filter_by.length;
+        let title_len = title.length;
+  
+        let start_idx = title_lower.indexOf(filter_by);
+        let end_idx = start_idx + filter_len;
+  
+        let start = title.substr(0,  start_idx );
+        let middle = title.substr(start_idx, filter_len);
+        let end = title.substr(end_idx, title_len);
+        highlighted_title = start + "<b>" + middle + "</b>" + end;
+      }
+    }
+
+    if (this.is_canceled){
+      highlighted_title = "<s>" + highlighted_title + "</s>"
+    }
+
+    return highlighted_title;
+  }
+
+  swapCancel(){
+    if (this.is_canceled){
+      this.is_canceled = false;
+    }else{
+      this.is_canceled = true;
+    }
+  }
+
+  setDisplay(){
+    // handles the logic whether the task should be displayed or not.
+    let filter_by = this.component.filter_by;
+    if (filter_by !== ""){
+      let title = this.title;
+      let title_lower = title.toLowerCase();
+      if (title_lower.includes(filter_by)){
+        this.displayable = true;
+      }else{
+        this.displayable = false;
+      }
+    }else{
+      this.displayable = true;
+    }
+  }
 }
 
 @Component({
@@ -13,65 +69,36 @@ interface Task{
 })
 export class AppComponent {
   tasks: Array<Task> = [
-    {
-      title: "Go home",
-      is_canceled: false,
-      f_idx: null
-    },
-    {
-      title:"Take a nap",
-      is_canceled: false,
-      f_idx: null
-    },
-    {
-      title: "Start learning Angular with Sabuj",
-      is_canceled: false,
-      f_idx: null
-    }
+    new Task("Go home", this),
+    new Task("Take a nap", this),
+    new Task("Start learning Angular with Sabuj", this)
   ];
-
-  filtered_tasks: Array<Task> = [];
 
   filter_by: string = "";
   
-  constructor(){
-    this.filterTasks();
-  }
-
   clearToDo(){
     let do_delete = confirm("Are you sure to delete all tasks?");
     if (do_delete){
       this.tasks.splice(0);
     }
-    this.filterTasks();
   }
 
   addTask(input){
     let value = input.value;
     input.value = "";
     this.tasks.push(
-      {
-        title: value,
-        is_canceled: false,
-        f_idx: null
-      });
-    this.filterTasks();
+      new Task(value, this)
+    );
   }
 
   cancelTask(idx: number){
-    if (this.tasks[idx].is_canceled){
-      this.tasks[idx].is_canceled = false;
-    }else{
-      this.tasks[idx].is_canceled = true;
-    }
-    this.filterTasks();
+    this.tasks[idx].swapCancel();
   }
 
   deleteTask(idx: number){
     let do_delete = confirm("Are you sure to delete the task?");
     if (do_delete){
       this.tasks.splice(idx, 1);
-      this.filterTasks();
     }
   }
 
@@ -80,52 +107,26 @@ export class AppComponent {
     let result = prompt("Edit Task Title", title);
     if (result !== null && result !== ""){
       this.tasks[idx].title = result;
-      this.filterTasks();
+      this.setTaskDisplays();
     }
   }
 
-  filterTasks(){
-    let filtered_tasks: Array<Task> = [];
-    for(let idx=0; idx < this.tasks.length; idx++){
-      let task = this.tasks[idx];
-      let title = task.title;
-      let title_lower = title.toLowerCase();
-      if (title_lower.includes(this.filter_by)){
-        let filter_len = this.filter_by.length;
-        let title_len = title.length;
-
-        let start_idx = title_lower.indexOf(this.filter_by);
-        let end_idx = start_idx + filter_len;
-
-        let start = title.substr(0,  start_idx );
-        let middle = title.substr(start_idx, filter_len);
-        let end = title.substr(end_idx, title_len);
-
-        let converted_title = start + "<b>" + middle + "</b>" + end;
-
-        filtered_tasks.push(
-          <Task>{
-            title: converted_title,
-            is_canceled: task.is_canceled,
-            f_idx: idx
-          }
-        );
-      }
+  private setTaskDisplays(){
+    for(let task of this.tasks){
+      task.setDisplay();
     }
-
-    this.filtered_tasks = filtered_tasks;
   }
 
   addFilter(filter_input){
     let filter_by: string = filter_input.value;
     filter_by = filter_by.toLowerCase();
     this.filter_by = filter_by;
-    this.filterTasks();
+    this.setTaskDisplays();
   }
 
   clearFilter(filterInput){
     filterInput.value = "";
     this.filter_by = "";
-    this.filterTasks();
+    this.setTaskDisplays();
   }
 }
